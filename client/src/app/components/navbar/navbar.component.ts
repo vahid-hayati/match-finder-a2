5f7a6acd9d74d39aa192306bcf28f11036ab3f22
vahid-hayati <vahidhayati68@gmail.com>
2025-09-15 21:07:53 +0330

    Designed mat menu for navbar

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,23 @@
 import { Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterModule } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 import { TestService } from '../../services/test.service';
 import { AccountService } from '../../services/account.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
+    CommonModule,
     RouterModule, RouterLink,
     MatButtonModule, MatToolbarModule, MatIconModule,
+    MatMenuModule, MatDividerModule, MatListModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
