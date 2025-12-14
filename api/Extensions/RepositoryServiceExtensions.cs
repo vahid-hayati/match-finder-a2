15a0d42a368f6ccdbed0bb46aac07b9388eae829
vahid-hayati <vahidhayati68@gmail.com>
2025-12-14 19:07:39 +0330

    Added photo service and photo modify service to .net DI

--- api/Extensions/RepositoryServiceExtensions.cs
+++ api/Extensions/RepositoryServiceExtensions.cs
@@ -1,17 +1,21 @@
 using api.Interfaces;
 using api.Repositoris;
 using api.Services;
+using Image_Processing_WwwRoot.Interfaces;
+using Image_Processing_WwwRoot.Services;
 
 namespace api.Extensions;
 
 public static class RepositoryServiceExtensions
 {
     public static IServiceCollection AddRepositoryService(this IServiceCollection services)
-    {        
+    {
         services.AddScoped<IAccountRepository, AccountRepository>();
         services.AddScoped<IMemberRepository, MemberRepository>();
         services.AddScoped<IUserRepository, UserRepository>();
-        services.AddScoped<ITokenService, TokenService>(); 
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IPhotoService, PhotoService>();
+        services.AddScoped<IPhotoModifySaveService, PhotoModifySaveService>();
 
         return services;
     }
