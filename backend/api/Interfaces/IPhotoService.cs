87e853691119d1d2684c776f089ec6727b98b4c5
realpj10101 <parsajfr1385oo@gmail.com>
2025-12-28 13:55:40 +0330

    create delete photo from disk interface

--- backend/api/Interfaces/IPhotoService.cs
+++ backend/api/Interfaces/IPhotoService.cs
@@ -1,6 +1,9 @@
+using api.Models;
+
 namespace api.Interfaces;
 
 public interface IPhotoService
 {
     public Task<string[]?> AddPhotoToDiskAsync(IFormFile file, string userId);
+    public Task<bool> DeletePhotoFromDiskAsync(Photo photo);
 }
